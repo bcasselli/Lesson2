241cb21ee6c5f5eea727f3d3c7c043b798270b4e
Brandon M Casselli <bc608@pvhs-lab112-d08.pvsd.org>
2016-09-08 14:29:29 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Declare variable:
+        //String or int or double
+        String s;
+        s = "name";//initialize
+        String s2 = ("nickname");
+        System.out.println(s);
+        //Naming rules of variables
+        //1. if, ele, void, public
+        //Avoid all reserved words
+        //2. Can't start a string with a number
+        // 1asdasfaf is not good!
+        //3. Special characters #@!$%^&*
+        //4. Can't have spaces
+        //  First Name
+        // firstName first_name
+        //5. Reuse variable names
+        //String vs int
+        //int vs double
+        int a =45;
+        double b = 3;
+        double sum = a + b;
+
+        a = 2_342_342;
+        int E,d = 0;
+        int sometime;
+        int thissssss = 0;
+        String samthing = "";
+        //String object pieces of it
+        //Strings have methods.
+    }
+}
